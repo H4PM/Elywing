d379dd78dd0732724c30215f82e2da6dbd9f31b3
SuperMaXAleX <marielolo93@gmail.com>
2016-12-27 10:53:19 +0100

    Added a new metadata flag. Thanks PMMP.
    Thaaaanks PMMP.

--- src/pocketmine/entity/Entity.php
+++ src/pocketmine/entity/Entity.php
@@ -152,12 +152,14 @@ abstract class Entity extends Location implements Metadatable{
 	const DATA_FLAG_TAMED = 24;
 	const DATA_FLAG_LEASHED = 25;
 	const DATA_FLAG_SHEARED = 26; //for sheep
-	const DATA_FLAG_FALL_FLYING = 27; //???
+	const DATA_FLAG_GLIDING = 27; DATA_FLAG_FALL_FLYING = 27; //for Elytras
 	const DATA_FLAG_ELDER = 28; //elder guardian
 	const DATA_FLAG_MOVING = 29;
 	const DATA_FLAG_BREATHING = 30; //hides bubbles if true
 	const DATA_FLAG_CHESTED = 31; //for mules?
-	const DATA_FLAG_STACKABLE = 32; //???
+	const DATA_FLAG_STACKABLE = 32;
+	
+	const DATA_FLAG_IDLING = 36; //Thanks pmmp.
 
 	const SOUTH = 0;
 	const WEST = 1;
@@ -438,6 +440,15 @@ abstract class Entity extends Location implements Metadatable{
 			$attr = $this->attributeMap->getAttribute(Attribute::MOVEMENT_SPEED);
 			$attr->setValue($value ? ($attr->getValue() * 1.3) : ($attr->getValue() / 1.3));
 		}
+	}
+	
+	public function isGliding(){
+		return $this->getDataFlag(self::DATA_FLAGS, self::DATA_FLAG_IDLING);
+	}
+
+	public function setGliding($value = true){
+		$this->setDataFlag(self::DATA_FLAGS, self::DATA_FLAG_FALL_FLYING, (bool) $value);
+		$this->setDataFlag(self::DATA_FLAGS, self::DATA_FLAG_IDLING, (bool) $value);
 	}
 
 	public function isImmobile() : bool{
