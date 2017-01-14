9ef5ef17787798d55769525ed8c67940b04e2347
Apple <ytapplegamer@gmail.com>
2017-01-14 14:00:40 -0500

    missed something

--- src/pocketmine/block/Block.php
+++ src/pocketmine/block/Block.php
@@ -259,6 +259,7 @@ class Block extends Position implements BlockIds, Metadatable{
 			self::$list[self::NETHER_BRICK_FENCE] = NetherBrickFence::class;
 			self::$list[self::POWERED_RAIL] = PoweredRail::class;
 			self::$list[self::RAIL] = Rail::class;
+			//self::$list[self::ENDER_CHEST] = EnderChest::class;
 			self::$list[self::WOODEN_PRESSURE_PLATE] = WoodenPressurePlate::class;
 			self::$list[self::STONE_PRESSURE_PLATE] = StonePressurePlate::class;
 			self::$list[self::LIGHT_WEIGHTED_PRESSURE_PLATE] = LightWeightedPressurePlate::class;
@@ -385,10 +386,10 @@ class Block extends Position implements BlockIds, Metadatable{
 	 *
 	 * @return bool
 	 */
-
 	public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
 		return $this->getLevel()->setBlock($this, $this, true, true);
 	}
+
 	/**
 	 * Returns if the item can be broken with an specific Item
 	 *
