a90a48ae5f65fa17c8e734c1e77064032b4ef471
Kenn <clashofklen@gmail.com>
2017-01-15 21:26:40 +0700

    Fix #393

--- src/pocketmine/item/Arrow.php
+++ src/pocketmine/item/Arrow.php
@@ -23,6 +23,10 @@ namespace pocketmine\item;
 
 class Arrow extends Item{
 	public function __construct($meta = 0, $count = 1) {
-		parent::__construct(self::ARROW, $meta, $count, "Arrow");
+		parent::__construct(self::ARROW, 0, $count, "Arrow");
+	}
+	
+	public function getMaxStackSize() : int {
+		return 64;
 	}
 }
