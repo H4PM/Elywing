2d97662efdeba3c72c10415f148a1bddcf13d2b0
SuperMaXAleX <marielolo93@gmail.com>
2017-02-11 11:17:26 +0100

    Update TransferPacket.php

--- src/pocketmine/network/protocol/TransferPacket.php
+++ src/pocketmine/network/protocol/TransferPacket.php
@@ -17,13 +17,20 @@
  *
  *
 */
+
 namespace pocketmine\network\protocol;
+
 #include <rules/DataPacket.h>
+
 class TransferPacket extends DataPacket{
+	
 	const NETWORK_ID = Info::TRANSFER_PACKET;
+	
 	public $address;
 	public $port = 19132;
+	
 	public function decode(){
+		
 	}
 	public function encode(){
 		$this->reset();
